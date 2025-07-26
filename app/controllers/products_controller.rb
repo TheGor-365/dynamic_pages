cdfc42a1bde4a8077768c7229a09a3ca55af5c97
Gor <7495500@gmail.com>
2025-07-26 23:32:03 +0300

    Modify routes, generate ProductsController, add actions

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,16 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js   # show.js.erb
+      format.turbo_stream
+      format.json { render json: @product }
+    end
+  end
+end
